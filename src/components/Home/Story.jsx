8d42844ee63879d4a796939c27dc00cef0b97380
Mehrab Tajik <mehrabt.pc@gmail.com>
2025-11-16 21:53:01 +0330

    Develop Home

--- src/components/Home/Story.jsx
+++ src/components/Home/Story.jsx
@@ -1,62 +1,49 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Popup from "./Popup";
 
 export default function Story() {
-  const [selectedStory,setSelectedStory] = useState(false)
+  const [selectedStory, setSelectedStory] = useState(false);
+  const [stories, setStories] = useState([]);
 
-  
+  async function getData() {
+    const response = await fetch("https://api.elinorboutique.com/v1/front/home");
+    const json = await response.json();
+    console.log(json.data.response.stories);
+
+    setStories(json.data.response.stories);
+  }
+
+  useEffect(() => {
+    getData();
+  }, []);
 
   return (
-      <div className="container mt-xxxx-large">
-          <Popup show={selectedStory} onClose={() => setSelectedStory(false)}>
-            <h2>سلام!</h2>
-            <p>این یک پاپاپ ساده ریکتی است :)</p>
-          </Popup>
+    <div className="container mt-xxxx-large">
+      <Popup show={selectedStory} onClose={() => setSelectedStory(false)}>
+        <h2>سلام!</h2>
+        <p>این یک پاپاپ ساده ریکتی است :)</p>
+      </Popup>
 
-        <div className="story-section my-3 text-center px-5">
-          <div className="row">
-            <div className="col-sm-12 col-md-12 col-lg-6 col-xl-6 d-flex justify-content-around align-items-center mb-3">
-              {/* Story Item:start */}
-              <div className="story-item" data-bs-toggle="modal" onClick={()=> setSelectedStory(true)} data-bs-target="#exampleModal" data-index="0">
-                <img src="assets/img/story-1.jpg" alt="" className="img-fluid border-radius-circle" />
-                <strong className="fs-7">سشوار</strong>
+      <div className="story-section my-3 text-center px-5">
+        <div className="row">
+          <div className="col-sm-12 col-md-12 col-lg-6 col-xl-6 d-flex justify-content-around align-items-center mb-3">
+            {stories.map((story, index) => (
+              <div
+                key={index}
+                className="story-item"
+                onClick={() => setSelectedStory(true)}
+              >
+                <img
+                  src={story.video_cover.url}
+                  alt={story.title}
+                  className="img-fluid border-radius-circle"
+                />
+                <strong className="fs-7">{story.title}</strong>
               </div>
-              {/* Story Item:end */}
-              <div className="story-item" data-bs-toggle="modal" data-bs-target="#exampleModal" data-index="1">
-                <img src="assets/img/story-2.jpg" alt="" className="img-fluid border-radius-circle" />
-                <strong className="fs-7">دکوری</strong>
-              </div>
-
-              <div className="story-item" data-bs-toggle="modal" data-bs-target="#exampleModal" data-index="2">
-                <img src="assets/img/story-3.jpg" alt="" className="img-fluid border-radius-circle" />
-                <strong className="fs-7">تخفیف</strong>
-              </div>
-
-              <div className="story-item" data-bs-toggle="modal" data-bs-target="#exampleModal" data-index="3">
-                <img src="assets/img/story-4.jpeg" alt="" className="img-fluid border-radius-circle" />
-                <strong className="fs-7">بهداشتی</strong>
-              </div>
-            </div>
-            <div className="col-sm-12 col-md-12 col-lg-6 col-xl-6 d-flex justify-content-around align-items-center mb-3">
-              <div className="story-item" data-bs-toggle="modal" data-bs-target="#exampleModal" data-index="4">
-                <img src="assets/img/story-5.jpeg" alt="" className="img-fluid border-radius-circle" />
-                <strong className="fs-7">وقت سفر</strong>
-              </div>
-              <div className="story-item" data-bs-toggle="modal" data-bs-target="#exampleModal" data-index="5">
-                <img src="assets/img/story-7.jpeg" alt="" className="img-fluid border-radius-circle" />
-                <strong className="fs-7">پیراهن</strong>
-              </div>
-              <div className="story-item" data-bs-toggle="modal" data-bs-target="#exampleModal" data-index="6">
-                <img src="assets/img/story-8.jpeg" alt="" className="img-fluid border-radius-circle" />
-                <strong className="fs-7">آرایش </strong>
-              </div>
-              <div className="story-item" data-bs-toggle="modal" data-bs-target="#exampleModal" data-index="7">
-                <img src="assets/img/story-6.jpeg" alt="" className="img-fluid border-radius-circle" />
-                <strong className="fs-7">برترین‌ها</strong>
-              </div>
-            </div>
+            ))}
           </div>
         </div>
       </div>
+    </div>
   );
 }
